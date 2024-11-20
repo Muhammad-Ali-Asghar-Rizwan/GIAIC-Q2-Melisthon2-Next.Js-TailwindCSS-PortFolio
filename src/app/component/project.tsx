2e09fa5b20ec685b1da0600c1d4ef1ecf3b1bf9e
Muhammad-Ali-Asghar-Rizwan <164878048+Muhammad-Ali-Asghar-Rizwan@users.noreply.github.com>
2024-11-20 18:47:21 +0500

    update

--- src/app/component/project.tsx
+++ src/app/component/project.tsx
@@ -4,7 +4,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/app/component/ui/card";
-
+// 
 import Link from "next/link";
 import Image from "next/image";
 import resume from "@/public/assets/project/resume.png";
